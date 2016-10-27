ba1c4b40a286f51ab50a31be06ea5b7160a5146a
CristianVera <crgvera@gmail.com>
2016-10-27 17:14:24 -0300

    Se crea una alianza y se consulta al usuario si desea continuar o salir

--- dominio/src/test/java/criterios_aceptacion/Historia12Test.java
+++ dominio/src/test/java/criterios_aceptacion/Historia12Test.java
@@ -1,6 +1,10 @@
 package criterios_aceptacion;
 
+import java.io.Console;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -34,7 +38,68 @@ public class Historia12Test {
 	@Test
 	public void historia12Criterio01_Test() throws FileNotFoundException{
 		
+		Long inicio = System.currentTimeMillis();
+		Alianza pro = crearAlianzaPro();
+		//Assert.assertEquals(2, pro.cantidadMiembrosAlianza());
+		Personaje p = new Humano("Mauricio Macri","123");
+		Scanner in = new Scanner(System.in);
+		System.out.println("¿Desea salir de la alianza?S/N: ");
+    	String respuesta = in.nextLine();
+    	if(respuesta.equals("S"))
+    	{
+        	pro.dejarAlianza(p);
+    		Assert.assertEquals(1, pro.cantidadMiembrosAlianza());
+    	}
+    	in.close();
+    	/*
+		Timer timer = new Timer();
+		TimerTask task = new TimerTask() {
+
+	        @Override
+	        public void run()
+	        {
+	        	Long fin = System.currentTimeMillis();
+	        	System.out.println(fin - inicio);
+	        	if(fin - inicio > 6000)
+	        	{
+		        	
+		    		timer.cancel();
+	        	}
+	        }
+	        };
+	    timer.schedule(task, 10, 1000);*/
+	}
+	
+	private Alianza crearAlianzaPro()
+	{
+		Personaje p1 = new Humano("Mauricio Macri","123");
+		Personaje p2 = new Humano("Eugenia Vidal","123");
+		p1.setClase(new Chaman());
+		p2.setClase(new Guerrero());
+		p1.setUbicacion(new Ubicacion(0, 0));
+		p2.setUbicacion(new Ubicacion(3, 0));
 		
+		/*
+		 * Creaciï¿½n de alianza
+		 * */
+		Alianza a = new Alianza("Pro");
+		
+		/*
+		 * Agrego personajes a la Alianza 
+		 * */
+		
+		a.formarAlianza(p1);
+		
+		for(Personaje p : a.getIntegrantes())
+		{
+			/*Verifico que estén cerca en un radio = 2*/
+			if(p2.seEncuentraCerca(p))
+			{
+				a.formarAlianza(p2);
+				break;
+			}
+		}
+		return a;
 	}
 	
 	
